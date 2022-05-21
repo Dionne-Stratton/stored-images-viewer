20d0520e9a58502869b4f02f603f4463b6df7736
Dionne <mylife33@hotmail.com>
2022-05-20 20:28:15 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -12,15 +12,21 @@ function App() {
   });
   return (
     <div className="App">
-      <h1>Image uploading react</h1>
+      <h1>Database Image Viewer</h1>
       {data.map((singleData) => {
         const base64String = btoa(
           String.fromCharCode(...new Uint8Array(singleData.img.data.data))
         );
         return (
-          <div>
-            <img src={`data:image/png;base64,${base64String}`} width="300"/>
-            <p>{singleData.title}</p>
+          <div className="aboutbox">
+            <img src={`data:image/png;base64,${base64String}`} alt={singleData.title} width="30%"/>
+            <div className="textbox">
+                <p>ID: {singleData._id}</p>
+                <p>Title: {singleData.title} | Size:{singleData.size} | Price: ${singleData.price}</p>
+                <p>Keywords: {singleData.keyword}</p>
+                <p>Description: {singleData.description}</p>
+                <a href={singleData.podLink}>Prints Link</a>
+            </div>
           </div>
         )
       })}
